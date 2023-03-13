a8bcd62546831872b900c7059513ed0fffd5aaa5
tianchang93824 <tianchang824@outlook.com>
2023-03-13 22:52:07 +0800

    修改word转pdfbug

--- word2pdf.py
+++ word2pdf.py
@@ -1,40 +1,41 @@
 # pip install comtypes -i https://pypi.tuna.tsinghua.edu.cn/simple/
 
 import os
-import comtypes.client
 from docx2pdf import convert
-from win32com import client
 from win32com.client import constants,gencache
+import shutil
 
 def get_path():
     # 获取当前运行路径
     # path = r'./'
 
     # 这里需要修改为你的word的文件夹路径
-    path = r'D:\trade\class\document\CBM'
+    path = r'D:\trade\class\document\CBM课堂测试word'
 
-    for path1 in os.listdir(path):
-        pathname = os.path.join(path,path1)
-        if os.path.isdir(pathname):
-            # 获取所有文件名的列表
-            filename_list = os.listdir(pathname)
-            # 获取所有word文件名列表
-            wordname_list = [filename for filename in filename_list \
-                             if filename.endswith((".doc", ".docx"))]
-            for wordname in wordname_list:
-                # 分离word文件名称和后缀，转化为pdf名称
-                pdfname = os.path.splitext(wordname)[0] + '.pdf'
-                # 如果当前word文件对应的pdf文件存在，则不转化
-                if pdfname in filename_list:
-                    continue
-                # 拼接 路径和文件名
-                wordpath = os.path.join(pathname, wordname)
-                pdfpath = os.path.join(pathname, pdfname)
-                # 生成器
-                yield wordpath, pdfpath
+    # 获取所有文件名的列表
+    filename_list = os.listdir(path)
+    # 获取所有word文件名列表
+    wordname_list = [filename for filename in filename_list \
+                     if filename.endswith((".doc", ".docx"))]
+    for wordname in wordname_list:
+        # 分离word文件名称和后缀，转化为pdf名称
+        pdfname = os.path.splitext(wordname)[0] + '.pdf'
+        # 如果当前word文件对应的pdf文件存在，则不转化
+        if pdfname in filename_list:
+            continue
+        # 拼接 路径和文件名
+        wordpath = os.path.join(path, wordname)
+        pdfpath = os.path.join(path, pdfname)
+        # 生成器
+        yield wordpath, pdfpath
 
 
 def doc2pdf():
+    #删除文件
+    genpath = os.path.join(os.getenv('temp'), 'gen_py')
+    if os.path.exists(genpath):
+        shutil.rmtree(genpath)
+
     word = gencache.EnsureDispatch("kwps.Application")  # 打开word应用程序
     #word = client.dynamic.Dispatch("kwps.Application")
     for wordpath, pdfpath in get_path():
